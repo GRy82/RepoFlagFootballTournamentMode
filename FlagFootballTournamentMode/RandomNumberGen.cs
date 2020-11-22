b72132d1a19078f597868b6d0b322cb379fd2412
GRy82 <gwryan@buffalo.edu>
2020-11-22 11:00:33 -0500

    Generate random number static method created. Will be primarily used for generating a random team

---
+++ FlagFootballTournamentMode/RandomNumberGen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlagFootballTournamentMode
+{
+    public class RandomNumberGen
+    {
+
+        public static int GenerateNumber(int lowerBound, int upperBound)
+        {
+            Random random = new Random();
+            int randGenNum = random.Next(lowerBound, upperBound + 1);
+            return randGenNum;
+        }
+    }
+}
